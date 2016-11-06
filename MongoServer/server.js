eb20b4a65d8b1adc898d167afd9ad2bc473f62f1
mohsen <mohsenhq@gmail.com>
2016-11-06 18:13:19 +0330

    post request

--- MongoServer/server.js
+++ MongoServer/server.js
@@ -3,8 +3,12 @@
 /**
  * Module dependencies.
  */
-
-
-
 var app = require('./config/lib/app');
 var server = app.start();
+var Mahtak = mongoose.model();
+
+app.post('/', function(req,res,next){
+  var u = new Mahtak(req.body)
+  db('Mahtak').save(u)
+  res.send(200)
+});
