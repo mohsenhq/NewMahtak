3d3c6c624496e72306715687c96cae36dc3f314f
mohsen <mohsenhq@gmail.com>
2016-11-09 14:25:14 +0330

    get body of post request and save it to the data database in mongodb

--- MongoServer/server.js
+++ MongoServer/server.js
@@ -1,14 +1,35 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-var app = require('./config/lib/app');
-var server = app.start();
-var Mahtak = mongoose.model();
-
-app.post('/', function(req,res,next){
-  var u = new Mahtak(req.body)
-  db('Mahtak').save(u)
-  res.send(200)
-});
+var mongodb = require('mongodb')
+var MongoClient = mongodb.MongoClient
+var url = 'mongodb://localhost:27017/data'
+const http = require('http')  
+const port = 80
+const requestHandler = (request, response) => {  
+var body = []
+request.on('data', function(chunk) {
+  body.push(chunk)
+}).on('end', function() {
+  body = Buffer.concat(body).toString()
+  bodyJson = JSON.parse(body)  
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err)
+  } else {
+    var collection = db.collection('data')
+    collection.insert(bodyJson, function (err, result) {
+      if (err) {
+        console.log(err)
+      }
+      db.close()
+    })
+  }
+})
+}) 
+  response.end('Done')
+}
+const server = http.createServer(requestHandler)
+server.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
