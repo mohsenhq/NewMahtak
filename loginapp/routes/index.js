6a03d2f695bc7299cf6399a475e991a46f0265fe
mohsen <mohsenhq@gmail.com>
2016-12-06 10:29:40 +0330

    add app and view apps order chenged

--- loginapp/routes/index.js
+++ loginapp/routes/index.js
@@ -3,14 +3,13 @@ var router = express.Router();
 var Application = require('../models/application');
 
 router.get('/', ensureAuthenticated, function(req, res){
-	res.render('index');
-});
-
-router.get('/applications', ensureAuthenticated, function(req, res){
 	Application.viewTable(res.locals.user.username, function(err, application){
 		if(err) throw err;
 		res.render('applications',{apps : application });
 	});
+});
+router.get('/addApp', ensureAuthenticated, function(req, res){
+	res.render('index');
 });
 
 function ensureAuthenticated(req, res, next){
