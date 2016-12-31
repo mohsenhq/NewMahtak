003bd63dc2388f58727e61e66be7216e30cf6056
mohsen <mohsenhq@gmail.com>
2016-12-31 14:55:42 +0330

    test method called from application page

--- loginapp/routes/index.js
+++ loginapp/routes/index.js
@@ -2,23 +2,28 @@ var express = require('express');
 var router = express.Router();
 var Application = require('../models/application');
 
-router.get('/', ensureAuthenticated, function(req, res){
-	Application.viewTable(res.locals.user.username, function(err, application){
-		if(err) throw err;
-		res.render('applications',{apps : application });
-	});
+router.get('/', ensureAuthenticated, function(req, res) {
+    Application.viewTable(res.locals.user.username, function(err, application) {
+        if (err) throw err;
+        res.render('applications', { apps: application });
+    });
 });
-router.get('/addApp', ensureAuthenticated, function(req, res){
-	res.render('index');
+router.get('/addApp', ensureAuthenticated, function(req, res) {
+    res.render('index');
 });
 
-function ensureAuthenticated(req, res, next){
-	if(req.isAuthenticated()){
-		return next();
-	} else {
-		// req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
+router.get('/test', ensureAuthenticated, function(req, res) {
+    // res.redirect('http://www.google.com');
+
+});
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        // req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
 }
 
 module.exports = router;
