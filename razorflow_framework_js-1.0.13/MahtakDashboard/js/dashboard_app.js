9d0f4b3e2a75ac4ac732fc0c681c92b7102a8cc2
mohsen <mohsenhq@gmail.com>
2017-01-16 17:06:48 +0330

    ajax code added

--- razorflow_framework_js-1.0.13/MahtakDashboard/js/dashboard_app.js
+++ razorflow_framework_js-1.0.13/MahtakDashboard/js/dashboard_app.js
@@ -1,11 +1,9 @@
-var a;
-$.ajax("http://localhost:8081/",{
+var a = $.ajax("http://localhost:8081/",{
     type: "GET",
       dataType: 'jsonp',
       data: {
       format: 'json'},   
     success: function (data) {
-    	a=data.parse(json);
         // rf.StandaloneDashboard(function (db) {
         //     // use the data from your 'data' variable to 
         //     // build the dashboard in the 'db' variable
