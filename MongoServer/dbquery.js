ab2674327061a38d0d875df436a00133b73bcf4e
mohsen <mohsenhq@gmail.com>
2017-01-17 14:01:02 +0330

    dbquery added

---
+++ MongoServer/dbquery.js
@@ -0,0 +1,53 @@
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+
+var http = require('http');
+var oooo=[];
+
+http.createServer(function(request, response) {
+  var headers = request.headers;
+  var method = request.method;
+  var url = request.url;
+  var body = [];
+  request.on('error', function(err) {
+    console.error(err);
+  }).on('data', function(chunk) {
+    body.push(chunk);
+  }).on('end', function() {
+    body = Buffer.concat(body).toString();
+    // BEGINNING OF NEW STUFF
+
+    response.on('error', function(err) {
+      console.error(err);
+    });
+
+    response.writeHead(200,{'Content-Type': 'application/json'},{'Access-Control-Allow-Origin': '*'});
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.writeHead(200, {'Content-Type': 'application/json'})
+    
+
+    // var responseBody = 
+    MongoClient.connect('mongodb://localhost:27017/data', function (err, db) {
+        if (err) {
+                    console.log('Unable to connect to the mongoDB server. Error:', err)
+        } else {
+                    var collection = db.collection('data');
+                    collection.find({}).sort({_id:-1}).limit(1).toArray(function(err, results)
+        {
+                    oooo= results;
+        });
+        }
+    });
+
+    response.write(JSON.stringify(oooo));
+    response.end();
+    // Note: the 2 lines above could be replaced with this next one:
+    // response.end(JSON.stringify(responseBody))
+
+    // END OF NEW STUFF
+  });
+}).listen(8088);
+
+
+
+
