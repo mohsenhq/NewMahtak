0834dbc573aae6582b83835f42fdd9dc40effe17
mohsen <mohsenhq@gmail.com>
2017-01-22 16:37:10 +0330

    dashboard added to the app

---
+++ loginapp/public/js/dashboard_app.js
@@ -0,0 +1,63 @@
+// Welcome to the RazorFlow Dashbord Quickstart. Simply copy this "dashboard_quickstart"
+// to somewhere in your computer/web-server to have a dashboard ready to use.
+// This is a great way to get started with RazorFlow with minimal time in setup.
+// However, once you're ready to go into deployment consult our documentation on tips for how to 
+// maintain the most stable and secure 
+
+$.ajax({
+    url: '/query',
+    type: 'POST',
+    data: '',
+    contentType: 'application/json; charset-utf-8',
+    // crossDomain: true,
+    dataType: 'json',
+    // header : {'Access-Control-Allow-Origin': '*'},
+    success: function(data) {
+        // console.log(data[0]);
+        // alert("success");
+        // $('#msg').html("Success");
+        test(data);
+    },
+    error: function(xhr, status, error) {
+        console.log(error);
+        // alert("fail");
+        // $('#msg').html("ERROR...! Please check the console (F12)");
+    }
+});
+
+function test(test) {
+    console.log(test[0].MainActivity);
+
+    StandaloneDashboard(function(db) {
+        // YOU CAN DELETE THE ENTIRE CONTENTS OF THIS FUNCTION AND CUSTOMIZE
+        // AS PER YOUR REQUIREMENT. 
+        // These components are simply here to give you a quick introduction of how RazorFlow Works
+
+        db.setDashboardTitle("My Dashboard");
+
+        // Add a chart to the dashboard. This is a simple chart with no customization.
+        var chart = new ChartComponent();
+        chart.setCaption("Sales");
+        chart.setDimensions(6, 6);
+        chart.setLabels(["1", "2", "3"]);
+        chart.addSeries([test[0].MainActivity, test[1].MainActivity, test[2].MainActivity]);
+        db.addComponent(chart);
+
+        // You can add multiple charts to the same dashboard. In fact you can add many 
+        // different types of components. Check out the docs at razorflow.com/docs 
+        // to read about all the types of components.
+        // 
+        // This is another chart with additional parameters passed to "addSeries" to 
+        // make customizations like change it to a line chart, and add "$" to indicate currency
+
+        // var chart2 = new ChartComponent();
+        // chart2.setCaption("Sales");
+        // chart2.setDimensions(6, 6);
+        // chart2.setLabels(["2013", "2014", "2015"]);
+        // chart2.addSeries([3151, 1121, 4982], {
+        //     numberPrefix: "$",
+        //     seriesDisplayType: "line"
+        // });
+        // db.addComponent(chart2);
+    });
+}
--- loginapp/routes/index.js
+++ loginapp/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var btoa = require('btoa');
 var router = express.Router();
 var Application = require('../models/application');
+
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
 
 router.get('/', ensureAuthenticated, function(req, res) {
     Application.viewTable(res.locals.user.username, function(err, application) {
@@ -17,6 +21,34 @@ router.get('/addApp', ensureAuthenticated, function(req, res) {
 router.get('/Build/*', ensureAuthenticated, function(req, res) {
     CallWebAPI(req.params[0]);
     res.redirect('/');
+});
+
+router.get('/MahtakDashboard', ensureAuthenticated, function(req, res) {
+    // CallWebAPI(req.params[0]);
+    // res.redirect(path.join(__dirname, '../public', 'index2.html'));
+    res.sendFile(path.join(__dirname, '../public', 'index2.html'));
+});
+
+router.post('/query', ensureAuthenticated, function(req, res) {
+    console.log("test2");
+    var oooo = [];
+    MongoClient.connect('mongodb://localhost:27017/data', function(err, db) {
+        if (err) {
+            console.log('Unable to connect to the mongoDB server. Error:', err)
+        } else {
+            var collection = db.collection('data');
+            collection.find({}).sort({ _id: -1 }).limit(10).toArray(function(err, results) {
+                oooo = results;
+                console.log(results);
+                res.write(JSON.stringify(results));
+                res.end();
+
+            });
+        }
+    });
+
+    // res.write(JSON.stringify(oooo));
+    // res.end();
 });
 
 function authenticateUser(user, password) {
