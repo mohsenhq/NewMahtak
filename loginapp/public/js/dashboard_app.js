0d89995b5ea45ac508d2ff7700b0ce0068638d45
mohsen <mohsenhq@gmail.com>
2017-01-25 17:10:24 +0330

    total users kpi added

--- loginapp/public/js/dashboard_app.js
+++ loginapp/public/js/dashboard_app.js
@@ -5,12 +5,20 @@ rf.StandaloneDashboard(function(db) {
     db.setDashboardTitle("APP Dashboard");
     var chart = new ChartComponent();
     chart.setCaption("App Usage and new Installs");
-    chart.setDimensions(12, 6);
-    chart.setYAxis('Usage');
-    chart.addYAxis('newInstalls', "newInstalls");
+    chart.setDimensions(10, 6);
+    chart.setYAxis('Number of Users');
+    // chart.addYAxis('newInstalls', "newInstalls");
 
     chart.lock();
     db.addComponent(chart);
+
+
+    var kpi = new KPIComponent();
+    kpi.setDimensions(2, 2);
+    kpi.setCaption("Total users");
+    kpi.lock();
+    db.addComponent(kpi);
+
 
     $.ajax({
         url: '/usageDate',
@@ -20,8 +28,13 @@ rf.StandaloneDashboard(function(db) {
         dataType: 'json',
         success: function(data) {
             log(data);
-            chart.setLabels(data.dates);
-            chart.addSeries('usage', 'Usage', data.sequences, { seriesDisplayType: 'line' });
+            if (data.dates.length < 31) {
+                chart.setLabels(data.dates);
+                chart.addSeries('usage', 'Usage', data.sequences, { seriesDisplayType: 'line' });
+            } else {
+                chart.setLabels(data.dates.slice(0, 30));
+                chart.addSeries('usage', 'Usage', data.sequences.slice(0, 30), { seriesDisplayType: 'line' });
+            }
         },
         error: function(xhr, status, error) {
             console.log(error);
@@ -36,11 +49,24 @@ rf.StandaloneDashboard(function(db) {
         dataType: 'json',
         success: function(data) {
             log(data);
-            chart.addSeries('newInstalls', 'New Installs', data.newInstalls, {
-                seriesDisplayType: 'line',
-                yAxis: 'newInstalls'
-            });
+            if (data.dates.length < 31) {
+                chart.addSeries('newInstalls', 'New Installs', data.newInstalls, {
+                    seriesDisplayType: 'line',
+                    yAxis: 'newInstalls'
+                });
+            } else {
+                chart.addSeries('newInstalls', 'New Installs', data.newInstalls.slice(0, 30), {
+                    seriesDisplayType: 'line'
+                });
+            }
             chart.unlock();
+            a = 0;
+            for (i = 0; i < data.newInstalls.length; i++) {
+                a += data.newInstalls[i];
+            }
+            kpi.setValue(a);
+            kpi.unlock();
+
         },
         error: function(xhr, status, error) {
             console.log(error);
